7dbe08b2b47f4140a581e495e97fb0bffdb196df
Parker Newton <techpedia101@gmail.com>
2015-05-18 18:00:19 -0700

    initial commit

---
+++ entropyCalculator.c
@@ -0,0 +1,35 @@
+/* 
+ * entropyCalculator.c 
+ * Parker Newton 
+ * 5-18-15 
+ * COEN 146L - T 2:15 pm 
+ */ 
+
+/* 
+ * This program calculates the entropy of a byte stream. 
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+#define NUM_CHARS 256
+
+int main(void) {
+	int i, count = 0; 
+	double entropy = 0.0, prob = 0.0; 
+	unsigned char buf;  
+	int freq[NUM_CHARS]; 
+	for(i = 0; i < NUM_CHARS; ++i)
+		freq[i] = 0; 
+	while(fscanf(stdin, "%hhu", &buf) == 1){  
+		++freq[buf]; 
+		++count; 
+	} 
+	for(i = 0; i < NUM_CHARS; ++i){
+		prob = (double)freq[i] / (double)count; 
+		entropy += prob * log2(1 / prob);   
+	}
+	printf("\nEntropy: %lf\n", entropy); 
+}
+
---
+++ randGenerator.c
@@ -0,0 +1,49 @@
+/* 
+ * randGenerator.c 
+ * Parker Newton 
+ * 5-18-15 
+ * COEN 146L - T 2:15 pm 
+ */ 
+
+/*
+ * This program generates a random byte stream 
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+#define SIZE 4000000
+#define NUM_CHARS 256
+
+int partition(int a[], int lo, int hi); 
+void quickSort(int a[], int lo, int hi); 
+
+int main(void) { 
+	unsigned char str[SIZE]; 
+	int freq[NUM_CHARS]; 
+	int i, r, freq_count, char_count; 
+	freq_count = SIZE / NUM_CHARS; 
+	if((NUM_CHARS * freq_count) != SIZE)
+		++freq_count; 
+	for(i = 0; i < NUM_CHARS; ++i) 
+		freq[i] = freq_count; 
+	srand(time(NULL));   
+	char_count = 0; 
+	for(i = 0; i < SIZE; ++i){
+		do {
+			r = abs(rand()) % NUM_CHARS;   
+		} while(freq[r] != freq_count);  
+		str[i] = r; 
+		--freq[r]; 
+		if(++char_count == NUM_CHARS){
+			char_count = 0; 
+			--freq_count; 
+		}
+	}
+	for(i = 0; i < SIZE; ++i){
+		fprintf(stdout, "\n%hhu \n", str[i]); 
+	}
+	return 0; 
+}
+
